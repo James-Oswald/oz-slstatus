fb19a0759eecb024a8b2925c36c0290a48bf0b94
James Oswald <jamesoswald111@gmail.com>
2023-05-15 07:12:36 +0000

    Customized bar

--- config.def.h
+++ config.def.h
@@ -64,6 +64,11 @@ static const char unknown_str[] = "n/a";
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  */
 static const struct arg args[] = {
-	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	/* function 		format          		argument */
+	{ datetime, 		"Time: %s | ",          "%F %T" },
+	{ battery_perc, 	"Battery: %s%% ",		"BAT0"},
+	{ battery_state, 	"%s | ",				"BAT0"},
+	{ ram_free,			"Mem r %s ",			NULL},
+	{ swap_total,		"s %s | ",				NULL},
+	{ wifi_essid,		"Network: %s ",			"wlp2s0"}
 };
--- config.mk
+++ config.mk
@@ -11,9 +11,10 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # flags
-CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE
-CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Wno-unused-parameter -Os
-LDFLAGS  = -L$(X11LIB) -s
+DEBUG = #-ggdb
+CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE $(DEBUG)
+CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Wno-unused-parameter -Os $(DEBUG)
+LDFLAGS  = -L$(X11LIB) $(DEBUG)
 # OpenBSD: add -lsndio
 # FreeBSD: add -lkvm -lsndio
 LDLIBS   = -lX11
